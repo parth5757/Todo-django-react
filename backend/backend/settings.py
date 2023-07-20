6b5e308f5cfc954fc756973ebc5901a7a104c97a
parth5757 <psthakkar2@gmail.com>
2023-07-20 19:45:57 +0530

    Todo 3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'todo',
+    'corsheaders',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'https://localhost:3000'
+)
 
 
 # Internationalization
