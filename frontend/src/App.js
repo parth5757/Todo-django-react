198ace20f4ea231e9d686da43156e7e231873d03
parth5757 <psthakkar2@gmail.com>
2023-07-25 08:48:42 +0530

    app.js, index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,104 +1,3 @@
-// import React, {Component} from 'react';
-// import './App.css';
-
-// const tasks = [
-//   {
-//     "id": 1,
-//     "title": "parth's web",
-//     "description": "-----",
-//     "completed": false
-//   },
-//   {
-//       "id": 2,
-//       "title": "Freelancing Work",
-//       "description": "freelancing",
-//       "completed": false
-//   },
-//   {
-//       "id": 3,
-//       "title": "Job Searching",
-//       "description": "search job on linkedin indeed nuakri",
-//       "completed": false
-//   },
-//   {
-//       "id": 4,
-//       "title": "Job Searching",
-//       "description": "search job on linkedin indeed nuakri",
-//       "completed": false
-//   },  
-// ]
-
-// class App extends Component {
-//   constructor(proops) {
-//     super(props);
-//     this.state={
-//       viewCompleted: false,
-//       taskList : tasks,
-//     };
-//   }
-
-//   displayCompleted = status => {
-//     if (status){
-//       return this.setstatus({ viewCompleted: true });
-//       }
-//       return this.setstatus({ viewCompleted: false });
-//   }
-
-//   renderTabList = () => {
-//     return(
-//       <div className="my-5-tab-list">
-//         <span
-//           onClick={()=> this.displayCompleted(true)}
-//           className={this.state.viewCompleted ? "active" : ""}
-//         >
-//           Completed
-//         </span>
-//                 <span
-//           onClick={()=> this.displayCompleted(false)}
-//           className={this.state.viewCompleted ? "active" : "active"} 
-//           Incompleted
-//         >
-//         </span>
-//       </div>
-//     )
-//   }
-
-//   renderItems = () =>{
-//     const { viewCompleted } = this.state;
-//     const newItems  = this.state.taskList.filter(
-//       item => item.completed == viewCompleted
-//     );
-//   };
-
-
-
-//   render(){
-//     return(
-//       <main className="context">
-//         <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
-//         <h1 className="row">
-//           <div className="col-md-6 col-sma-10 mx-auto p-0">
-//             <div className="card p-3">
-//               <button className="btn btn-warning">Add Task</button>
-
-//             </div>
-
-//           </div>
-
-//         </h1>
-
-
-
-//       </main>
-//     )
-//   }
-
-
-// }
-
-// export default App;
-
-
 import React, { Component } from 'react';
 import './App.css';
 
@@ -131,7 +30,7 @@ const tasks = [
 ];
 
 class App extends Component {
-  constructor(props) { // Fixed typo 'proops' to 'props'
+  constructor(props) {
     super(props);
     this.state = {
       viewCompleted: false,
@@ -140,11 +39,7 @@ class App extends Component {
   }
 
   displayCompleted = (status) => {
-    if (status) {
-      this.setState({ viewCompleted: true }); // Fixed setState
-    } else {
-      this.setState({ viewCompleted: false }); // Fixed setState
-    }
+    this.setState({ viewCompleted: status });
   }
 
   renderTabList = () => {
@@ -168,35 +63,28 @@ class App extends Component {
 
   renderItems = () => {
     const { viewCompleted } = this.state;
-    const newItems  = this.state.taskList.filter(
-      item => item.completed == viewCompleted
+    const newItems = this.state.taskList.filter(
+      item => item.completed === viewCompleted
     );
 
-    return newItems.map(item=>(
+    return newItems.map(item => (
       <li
-      key = {item.id}
-      className="list-group-ite d flex justify-content-between align-items-center">
-      
-        <span  className={'todo-title mr-2  ${this.state.viewCompleted ? "completed-todo : "" }'}
-        title={item.title}>
-        {item.title}
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center">
+        <span className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`} title={item.title}>
+          {item.title}
           <button className="btn btn-info mr-2">Edit</button>
           <button className="btn btn-danger mr-2">Delete</button>
         </span>
       </li>
-    ))  
-
+    ));
   };
-
-
-  
-
 
   render() {
     return (
       <main className="context">
         <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
-        <h1 className="row">
+        <div className="row">
           <div className="col-md-6 col-sma-10 mx-auto p-0">
             <div className="card p-3">
               <button className="btn btn-warning">Add Task</button>
@@ -206,7 +94,7 @@ class App extends Component {
               </ul>
             </div>
           </div>
-        </h1>
+        </div>
       </main>
     )
   }
